862b5b9e301e97b8ed132ac7b9a220efce956d90
syndercombecreswell <syndercombecreswell@gmail.com>
2022-11-15 15:46:44 +0000

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,57 @@
 import { useState } from "react";
 
-interface Joke {
-  id: number;
-  type: string;
-  setup: string;
-  punchline: string;
+
+interface Dog {
+  message: string;
+  status: string;
 }
+//{"message":"https:\/\/images.dog.ceo\/breeds\/terrier-westhighland\/n02098286_577.jpg","status":"success"} response
 
 function App() {
-  const [joke, setJoke] = useState<Joke>();
 
-  const handleGetJoke = async () => {
+  const [manyDogUrl,setManyDogUrl] = useState<string[]>([]);
+  const addDog = (newDog: string) => {
+    setManyDogUrl(prev => [...prev,newDog]);
+  }
+  
+  
+  
+  const [dogUrl,setDogUrl] = useState<string>();
+
+  const handleGetDog = async () => {
     const response = await fetch(
-      "https://jokestemp.neillbogie.repl.co/jokes/general/random"
+      "https://dog.ceo/api/breeds/image/random"
+
     );
-    const jsonBody: Joke[] = await response.json();
-    setJoke(jsonBody[0]);
+    const returnedObject: Dog = await response.json();
+    setDogUrl(returnedObject.message);
+    addDog(returnedObject.message);
   };
 
-  // const handleGetJoke = () => {
-  //   fetch("https://jokestemp.neillbogie.repl.co/jokes/general/random")
-  //     .then((response) => response.json())
-  //     .then((jsonBody: Joke[]) => setJoke(jsonBody[0]));
-  // };
-
-  if (joke) {
+ 
     return (
       <div>
         <h1>Joke app</h1>
+        <details>{ dogUrl &&
+          <img src={dogUrl} alt="dog" />
+}
+          </details>
+
         <details>
-          <summary>{joke.setup}</summary>
-          <p>{joke.punchline}</p>
+          {
+            manyDogUrl.map(doggy => {
+              return(
+              <div>
+                <img src={doggy} alt="dog" />
+              </div>
+            )})
+          }
         </details>
         <hr />
-        <button onClick={handleGetJoke}>Get another joke</button>
+        <button onClick={handleGetDog}>Get another joke</button>
       </div>
     );
-  } else {
-    return (
-      <div>
-        <h1>Joke app</h1>
-        <p>
-          Click the button to trigger a <code>fetch</code> that gets a random
-          joke from an API!
-        </p>
-        <button onClick={handleGetJoke}>Get joke</button>
-      </div>
-    );
-  }
-}
+  } 
+
 
 export default App;
